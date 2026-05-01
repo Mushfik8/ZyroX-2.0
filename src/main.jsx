50da6323220f2bc9b9c2b56f6eb26d97b44101ad
Mushfikur Rahman <129661198+Mushfik8@users.noreply.github.com>
2026-05-01 23:02:14 +0600

    Implement maintenance mode in main.jsx
    Added a maintenance mode that displays a message when the site is under maintenance.

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const maintenance = true;
+
+const Maintenance = () => (
+  <div style={{
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontFamily: "sans-serif",
+    flexDirection: "column"
+  }}>
+    <h1>🚧 Site Under Maintenance</h1>
+    <p>We’ll be back in 1 hour</p>
+  </div>
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  maintenance ? <Maintenance /> : <App />
 )
